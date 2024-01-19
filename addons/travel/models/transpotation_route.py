0b382e2ed7812c507ea9a30a11e83a075ff1c68f
wint-phyusin <gnext.wintphyusinmaung@gmail.com>
2024-01-19 15:59:24 +0630

    add: route

--- addons/travel/models/transpotation_route.py
+++ addons/travel/models/transpotation_route.py
@@ -6,4 +6,11 @@ class TranspotationRoute(models.Model):
     
     travel_agency_id = fields.Many2one('travel.agency',required=True)
     travel_car_id = fields.Many2one('travel.car')
+    avatar = fields.Image(related='travel_car_id.avatar')
     car_driver = fields.Many2one('res.partner',related="travel_car_id.car_driver")
+    start_datetime = fields.Datetime()
+    start_township = fields.Many2one('travel.township')
+    start_gate = fields.Many2one('travel.gate')
+    arrive_datetime = fields.Datetime()
+    arrive_township = fields.Many2one('travel.township')
+    arrive_gate = fields.Many2one('travel.gate')
